0fd975574f4be4a48bcee295596d203167db077f
Nabeel Rana <nabwaregroup@gmail.com>
2023-09-26 14:55:39 -0700

    Completed serial generator

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +17,17 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+    def __init__(self, start):
+        """ Initializes SerialGenerator object with start and current value """
+        self.start = start
+        self.next = self.start
+
+    def generate(self):
+        """ Increments current value by 1 and returns current value """
+        self.next += 1
+        return self.next - 1
+
+    def reset(self):
+        """ Resets current value to start value """
+        self.next = self.start
