6c8303316ce11bd4434e949ca4bf180275c88042
Nabeel Rana <nabwaregroup@gmail.com>
2023-09-26 16:58:18 -0700

    Completed random word

--- wordfinder.py
+++ wordfinder.py
@@ -1,22 +1,40 @@
+from random import choice
+
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
 
     def __init__(self, file_path):
         """ Initializes a wordfinder object which creates a list of words read
          from a given file path and prints out the number of words read """
-        self.file_path = file_path
-        self.word_list = self.get_word_list()
 
+        self.words = self.get_words(file_path)
 
-    def get_word_list(self):
+        print(f"{len(self.words)} words read")
+
+    def get_words(self, file_path):
         """ read a file at the given file path and return a list of words """
 
-        word_list = []
-        file = open(self.file_path, 'r')
+        file = open(file_path, 'r')
 
-        for line in file:
-            word_list.append(line.strip('\n'))
+        words = [line.strip() for line in file]
 
         file.close()
 
-        return word_list
+        return words
+
+    def random(self):
+        """ Returns random word from list of words """
+
+        return choice(self.words)
+        # return set(self.word_list).pop()
+
+
+class SpecialWordFinder(WordFinder):
+    """Random Word Finder: handles # and blank space cases"""
+
+    def get_words(self, file_path):
+        """ Filters out lines that start with # and blank lines """
+
+        dirty_words = super().get_words(file_path)
+
+        return [word for word in dirty_words if word and not word.startswith("#")]
